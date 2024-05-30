c64708ee0c17d6552e59ac29f540bd74d99dd8bc
07XYGIN <1270784682@qq.com>
2024-05-31 03:56:59 +0800

    '解决ts类型报错问题'

--- src/commponts/Cards.tsx
+++ src/commponts/Cards.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Card } from 'antd';
-import { Link } from 'react-router-dom';
+import { Link, To } from 'react-router-dom';
 import "../Style/Card/Card.scss"
 interface CardProps {
-  cards: object;
+  cardlist: any;
 }
 const App: React.FC<CardProps> = ({cardlist}) => {
 
-  const listItems = cardlist.map(item =>
+  const listItems:CardProps = cardlist.map((item: { id: React.Key | null | undefined; href: To; title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; text: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; time: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }) =>
     <Link key={item.id} to={item.href} data-Cards-a>
       <Card title={item.title} bordered={false} style={{ width: 300,backgroundColor:'var(--background-color)' }}>
         <p>{item.text}</p>
